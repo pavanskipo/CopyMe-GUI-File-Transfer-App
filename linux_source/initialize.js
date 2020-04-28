ec99632959f9fa593b6162d776637a6b1aadf5d5
Pavan <pavanskipo@gmail.com>
2020-04-28 18:09:55 +0530

    Adding check for existing packages

--- linux_source/initialize.js
+++ linux_source/initialize.js
@@ -1,18 +1,27 @@
-
-var sudo = require('sudo-prompt');
+const sudo = require('sudo-prompt');
+const {
+  exec
+} = require("child_process");
 
 var options = {
   name: 'CopyME',
 };
 
-module.exports = function() {
-  let path = process.cwd() + '/linux_source/install_dependencies.sh';
-  sudo.exec('sh ' + path, options,
-    function(error, stdout, stderr) {
-      if(stdout.includes("checked")) {
-        console.log("Intialization complete");
-      }
-      if (error) throw error
+module.exports = function () {
+  exec("dpkg-query -W -f='${Status} ${Version}\n' nmap | grep 'installed'", (error, stdout, stderr) => {
+    if (stdout.includes("installed")) {
+      return true;
+    } else {
+      console.log("Installing dependencies, please wait...")
+      let path = process.cwd() + '/linux_source/install_dependencies.sh';
+      sudo.exec('sh ' + path, options,
+        function (error, stdout, stderr) {
+          if (stdout.includes("checked")) {
+            console.log("Intialization complete");
+          }
+          if (error) throw error
+        }
+      );
     }
-  );  
+  });
 }
